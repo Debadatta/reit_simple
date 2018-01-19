77d12018f2308f53eafb1b4e65a63f32076d83e2
debadatta <debadattapradhan@gmail.com>
2018-01-19 09:55:43 +0530

    Added home page

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,5 @@
+class HomeController < ApplicationController
+	skip_before_filter :authenticate_request
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
   post '/authentication/login' => 'authentication#login'
+  root :to => "home#index"
 end
