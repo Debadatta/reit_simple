588b0de8e0d68ca9d06b2af1cc85515014ec5a52
Sukanta-kreeti <smangal@kreeti.com>
2018-01-24 01:03:44 +0530

    added about us page

---
+++ web/src/components/aboutUs/Investors.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+export default class Investors extends React.PureComponent {
+  render() {
+    return (
+      <section className="logo-grid">
+            <div className="container">
+              <h2>These Are Our Investors</h2>
+              <a href="http://www.khoslaventures.com/">
+                <img width={207} height={26} src="https://www.roofstock.com/wp-content/uploads/2016/06/khosla.png" className="attachment-medium size-medium" alt="khosla" />                  </a>
+              <a href="http://www.baincapitalventures.com/">
+                <img width={207} height={51} src="https://www.roofstock.com/wp-content/uploads/2016/06/bain-ventures-logo.png" className="attachment-medium size-medium" alt="bain-ventures-logo" />                  </a>
+              <a href="http://lsvp.com/">
+                <img width={770} height={126} src="https://www.roofstock.com/wp-content/uploads/2016/06/lightspeed-logo-770x126.png" className="attachment-medium size-medium" alt="Lightspeed Venture Partners" />                  </a>
+              <a href="http://www.canvas.vc/">
+                <img width={770} height={186} src="https://www.roofstock.com/wp-content/uploads/2016/06/Canvas_300dpiLogo-770x186.png" className="attachment-medium size-medium" alt="Canvas Ventures logo" />                  </a>
+              <a href="http://www.nycapartners.com/">
+                <img width={157} height={50} src="https://www.roofstock.com/wp-content/uploads/2016/06/nyca.png" className="attachment-medium size-medium" alt="nyca" />                  </a>
+              <a href="http://qedinvestors.com/">
+                <img width={106} height={56} src="https://www.roofstock.com/wp-content/uploads/2016/06/qed.png" className="attachment-medium size-medium" alt="qed" />                  </a>
+              <a href="https://angel.co/csc-upshot">
+                <img width={132} height={40} src="https://www.roofstock.com/wp-content/uploads/2016/06/csc-upshot.png" className="attachment-medium size-medium" alt="csc-upshot" />                  </a>
+              <a href="http://www.greywolfcapital.com/">
+                <img width={199} height={42} src="https://www.roofstock.com/wp-content/uploads/2016/06/greywolf.png" className="attachment-medium size-medium" alt="greywolf" />                  </a>
+              <a href="https://www.crunchbase.com/person/marc-benioff#/entity">
+                <img width={148} height={25} src="https://www.roofstock.com/wp-content/uploads/2016/06/Marc-Benioff.png" className="attachment-medium size-medium" alt="Marc Benioff" />                  </a>
+              <a href>
+              </a>
+            </div>
+          </section>
+    )
+  }
+}
