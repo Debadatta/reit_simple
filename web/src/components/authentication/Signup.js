1cccc84563ba03d529cec667b3ec918cacea24da
Sukanta-kreeti <smangal@kreeti.com>
2018-03-06 22:12:58 +0530

    typo

--- web/src/components/authentication/Signup.js
+++ web/src/components/authentication/Signup.js
@@ -270,7 +270,7 @@ class Signup extends Component {
                                 <div className="rs-form-group text-center termsContainer">
                                   <small>
                                     By clicking Sign Up or registering, I agree to
-                                    <Link to="/terms"> REITSimple&#44;s Terms &amp; Conditions</Link>
+                                    <Link to="/terms"> REITSimple&#39;s Terms &amp; Conditions</Link>
                                   </small>
                                 </div>
                               </ActiveForm>
