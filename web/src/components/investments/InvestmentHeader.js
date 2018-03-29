8cf2832b5f1ac709e3716a5c343240e4aea1a8d2
Sukanta Kumar Mangal <smangal@kreeti.com>
2018-03-29 20:56:21 +0530

    made my investments tabs clickable (#35)

--- web/src/components/investments/InvestmentHeader.js
+++ web/src/components/investments/InvestmentHeader.js
@@ -1,14 +1,18 @@
 import React from 'react';
 
 export default class InvestmentHeader extends React.Component {
+  getActiveClass(tab) {
+    return this.props.currentTab === tab ? "active" : "";
+  }
+
   render() {
     return (
       <div className="inverstments-header">
         <h3>My Investments</h3>
         <div id="tabs-secondary" className="mar-t-30">
           <ul className="nav nav-tabs" role="tablist">
-            <li><span>In Contract</span></li>
-            <li className="active"><span>Owned Properties</span></li>
+            <li onClick={this.props.onTabChange.bind(null, "contract")} className={this.getActiveClass("contract")}><span>In Contract</span></li>
+            <li onClick={this.props.onTabChange.bind(null, "myInvest")} className={this.getActiveClass("myInvest")}><span>Owned Properties</span></li>
           </ul>
         </div>
       </div>
--- web/src/components/investments/MyInvestments.js
+++ web/src/components/investments/MyInvestments.js
@@ -3,24 +3,53 @@ import InvestmentHeader from "./InvestmentHeader";
 import '../../styles/investments.css';
 
 export default class MyInvestments extends React.Component {
+  state = {
+    currentTab: "myInvest"
+  }
+
+  handleTabChange = (currentTab, e) => this.setState({currentTab});
+
+  renderNoContent() {
+    return (
+      <div className="no-investments-container">
+        <div className="finding-roofs-full">
+          <div className="title">You do not have any investments yet</div>
+          <div className="seller-actions">
+            <div className="action">
+              <a href="/investment-property-marketplace">Browse properties</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+  renderMyInvestments() {
+    return this.renderNoContent();
+  }
+
+  renderContracts() {
+    return null;
+  }
+
+  renderContent() {
+    switch(this.state.currentTab) {
+    case "contract":
+      return this.renderContracts();
+    default:
+      return this.renderMyInvestments();
+    }
+  }
+
   render() {
     return (
       <section className="body-content adjust-body container investments">
-        <InvestmentHeader/>
+        <InvestmentHeader onTabChange={this.handleTabChange} currentTab={this.state.currentTab}/>
         <div className="investments-content">
           <div className="table-responsive">
             <table className="table properties">
               <tbody>
-                <div className="no-investments-container">
-                  <div className="finding-roofs-full">
-                    <div className="title">You do not have any listings yet</div>
-                    <div className="seller-actions">
-                      <div className="action">
-                        <a href="/investment-property-marketplace">Browse properties</a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
+                {this.renderContent()}
               </tbody>
             </table>
           </div>
